22c697c8d6da0ba681f060bf90a80290ed36b143
Terry <123653821@qq.com>
2014-10-13 13:33:51 -0400

    1

--- mysql.php
+++ mysql.php
@@ -1,13 +1,5 @@
 <?php
-    $db_host    = 'localhost:8080';
-    $db_username= 'root';
-    $db_password= 'toor';
-    $db_database= 'ruhungry';
- 
-        $db = mysql_connect($db_host, $db_username, $db_password);
-		if (!$db)
-	  {
-	  die('Could not connect: ' . mysql_error());
-	  }
-      mysql_select_db($db_database, $db);
+//Connect to Database
+mysql_connect("localhost", "root", "toor") or die(mysql_error());
+mysql_select_db("ruhungry") or die(mysql_error());
 ?>
