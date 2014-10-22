b8c1076ad7f3ec2935e836419ace3f3f97171228
FRoselleck <Zareklinz@gmail.com>
2014-10-22 19:48:36 -0400

    orderConfirm

--- orderConfirm.php
+++ orderConfirm.php
@@ -1,6 +1,6 @@
 <?php
 include "mysql.php";
-include "cart_add.php";
+// include "cart_add.php";
 session_start();
 
 $con = mysql_connect("localhost","root","toor");
@@ -9,7 +9,73 @@ if (!$con)
 		die('Could not connect: ' . mysql_error());
 	}
 mysql_select_db("ruhungry", $con);
-$tempname=$_SESSION["username"];
+$username=$_SESSION["username"];
+date_default_timezone_set('UTC');
+
+
+		if(isset($_POST['op_Bill_Number']))
+		{
+			$sql_select = 'select inventory_info.Item_Name, inventory_info.Item_ID, restaurant_info.Restrt_Name, shopping_cart_info.Quantity, inventory_info.Price from shopping_cart_info 
+			left join inventory_info on shopping_cart_info.Item_ID = inventory_info.Item_ID left join restaurant_info on shopping_cart_info.Restrt_ID = restaurant_info.Restrt_ID where Username ="'.$username.'"';
+			$result = mysql_query($sql_select) or die("sql error!");
+			$time = date('ymdhis',time());
+			while($row = mysql_fetch_array($result))
+			{
+				$total=($row[Price]*$row[Quantity]);
+				$sql_insert="INSERT INTO checkout_info (OrderNO,Username,Item_ID,Restrt_ID,Card_Account,Shipping_Address,Checkout_Date,Price)
+					VALUES
+				('$time_$username','$username','$row[Item_ID]','$row[Item_ID]','$_POST[op_Bill_Number]','$_POST[op_Ship_First_Name]| |$_POST[op_Ship_Last_Name]| |$_POST[op_Ship_Address1]| |$_POST[op_Ship_Address2]| |$_POST[op_Ship_City]| |$_POST[op_Ship_State]| |$_POST[op_Ship_Zip]','$time','$total')";
+			if (!mysql_query($sql_insert,$con))
+					die('Error: ' . mysql_error());
+			}
+			echo "<script> alert('Success!');   window.location.href('index.php');</script>";
+		}
+
+
+
+
+
+
+	function cartReview($username){
+		if(isset($username)){
+			//echo "worked!";
+			$sql_select = 'select inventory_info.Item_Name, inventory_info.Item_ID, restaurant_info.Restrt_Name, shopping_cart_info.Quantity, inventory_info.Price from shopping_cart_info 
+			left join inventory_info on shopping_cart_info.Item_ID = inventory_info.Item_ID left join restaurant_info on shopping_cart_info.Restrt_ID = restaurant_info.Restrt_ID where Username ="'.$username.'"';
+		}
+		else{ echo "No user!";}
+		$result = mysql_query($sql_select) or die("sql error!");
+
+		while($row = mysql_fetch_array($result)){
+			//print html form of the shopping cart
+			$outputlist = "";
+			$outputlist.= '<tr>';
+			$outputlist.= '<td >';
+			$outputlist.= '<h4>'.$row["Restrt_Name"].'</h4>';
+			$outputlist.= '</td>';
+			$outputlist.= '<td>';
+			$outputlist.= '<p>'.$row["Item_Name"].'</p>';
+			$outputlist.= '</td>';
+			$outputlist.= '<td class="cart_price">';
+			$outputlist.= '<p>'.$row["Price"].'</p>';
+			$outputlist.= '</td>';
+			$outputlist.= '<td class="cart_price">';
+			$outputlist.= '<p>'.$row["Quantity"].'</p>';
+			$outputlist.= '</td>';
+			$outputlist.= '<td class="cart_total">';
+			$outputlist.= '<p class="cart_total_price">'.($row["Price"]*$row["Quantity"]).'</p>';
+			$outputlist.= '</td>';
+			$outputlist.= '<td>';
+			$outputlist.= '<form action="cart_delete.php" method="post">  <input name="Item_Name" value="'.$row["Item_ID"].'" style="display:none;">';
+			$outputlist.= '<input name="Username" value="'.$username.'" style="display:none;">';
+			$outputlist.= '</form>';
+			$outputlist.= '</td>';
+			$outputlist.= '</tr>';
+
+			print $outputlist;
+		}
+	}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -42,6 +108,25 @@ $tempname=$_SESSION["username"];
 	<script type="text/javascript">
 	function pay()
 	{
+		document.getElementById("op_Ship_First_Name").value=document.getElementById("Ship_First_Name").value;
+		document.getElementById("op_Ship_Last_Name").value=document.getElementById("Ship_Last_Name").value;
+		document.getElementById("op_Ship_Address1").value=document.getElementById("Ship_Address1").value;
+		document.getElementById("op_Ship_Address2").value=document.getElementById("Ship_Address2").value;
+		document.getElementById("op_Ship_City").value=document.getElementById("Ship_City").value;
+		document.getElementById("op_Ship_State").value=document.getElementById("Ship_State").value;
+		document.getElementById("op_Ship_Zip").value=document.getElementById("Ship_Zip").value;
+		document.getElementById("op_Bill_First_Name").value=document.getElementById("Bill_First_Name").value;
+		document.getElementById("op_Bill_Last_Name").value=document.getElementById("Bill_Last_Name").value;
+		document.getElementById("op_Bill_Address1").value=document.getElementById("Bill_Address1").value;
+		document.getElementById("op_Bill_Address2").value=document.getElementById("Bill_Address2").value;
+		document.getElementById("op_Bill_City").value=document.getElementById("Bill_City").value;
+		document.getElementById("op_Bill_State").value=document.getElementById("Bill_State").value;
+		document.getElementById("op_Bill_Zip").value=document.getElementById("Bill_Zip").value;
+		document.getElementById("op_Bill_Type").value=document.getElementById("Bill_Type").value;
+		document.getElementById("op_Bill_Number").value=document.getElementById("Bill_Number").value;
+		document.getElementById("op_Bill_Holder").value=document.getElementById("Bill_Holder").value;
+		document.getElementById("op_Bill_Expire").value=document.getElementById("Bill_Expire").value;
+		document.getElementById("Ship_Info").submit();
 
 	}
 
@@ -72,29 +157,6 @@ $tempname=$_SESSION["username"];
 	</header>
 
 	<section id="cart_items" style="padding-top:30px">
-<!--
-			<div class="step-one">
-				<h2 class="heading">Step1</h2>
-			</div>
-			<div class="checkout-options">
-				<h3>New User</h3>
-				<p>Checkout options</p>
-				<ul class="nav">
-					<li>
-						<label><input type="checkbox"> Register Account</label>
-					</li>
-					<li>
-						<label><input type="checkbox"> Guest Checkout</label>
-					</li>
-					<li>
-						<a href=""><i class="fa fa-times"></i>Cancel</a>
-					</li>
-				</ul>
-			</div><!--/checkout-options-->
-
-			<!-- <div class="register-req">
-				<p>Please use Register And Checkout to easily get access to your order history, or use Checkout as Guest</p>
-			</div><!--/register-req-->
 		<div class="container">
 			<div class="breadcrumbs">
 				<ol class="breadcrumb">
@@ -110,13 +172,13 @@ $tempname=$_SESSION["username"];
 						<div class="shopper-info">
 							<p>Shipping Information</p>
 							<form id="Ship_Info" disabled＝"true" readonly＝"true">
-								<input disabled＝"true" readonly＝"true" id="Ship_First_Name" name="Ship_First_Name" type="text" value="First Name: <?php echo $_POST['fr_Ship_First_Name'] ?>">
-								<input disabled＝"true" readonly＝"true" id="Ship_Last_Name" name="Ship_Last_Name" type="text" value="Last Name: <?php echo $_POST['fr_Ship_Last_Name'] ?>">
-								<input disabled＝"true" readonly＝"true" id="Ship_Address1" name="Ship_Address1" type="text" value="Address1: <?php echo $_POST['fr_Ship_Address1'] ?>">
-								<input disabled＝"true" readonly＝"true" id="Ship_Address2" name="Ship_Address2" type="text" value="Address2: <?php echo $_POST['fr_Ship_Address2'] ?>">
-								<input disabled＝"true" readonly＝"true" id="Ship_City" name="Ship_City" type="text" value="City: <?php echo $_POST['fr_Ship_City'] ?>">
-								<input disabled＝"true" readonly＝"true" id="Ship_State" name="Ship_State" type="text" value="State: <?php echo $_POST['fr_Ship_State'] ?>">
-								<input disabled＝"true" readonly＝"true" id="Ship_Zip" name="Ship_Zip" type="text" value="Zip: <?php echo $_POST['fr_Ship_Zip'] ?>">
+								<input disabled＝"true" readonly＝"true" id="Ship_First_Name" name="Ship_First_Name" type="text" placehold="First Name" value="<?php echo $_POST['fr_Ship_First_Name'] ?>">
+								<input disabled＝"true" readonly＝"true" id="Ship_Last_Name" name="Ship_Last_Name" type="text" placehold="Last Name" value="<?php echo $_POST['fr_Ship_Last_Name'] ?>">
+								<input disabled＝"true" readonly＝"true" id="Ship_Address1" name="Ship_Address1" type="text" placehold="Address1" value="<?php echo $_POST['fr_Ship_Address1'] ?>">
+								<input disabled＝"true" readonly＝"true" id="Ship_Address2" name="Ship_Address2" type="text" placehold="Address2" value="<?php echo $_POST['fr_Ship_Address2'] ?>">
+								<input disabled＝"true" readonly＝"true" id="Ship_City" name="Ship_City" type="text" placehold="City" value="<?php echo $_POST['fr_Ship_City'] ?>">
+								<input disabled＝"true" readonly＝"true" id="Ship_State" name="Ship_State" type="text" placehold="State" value="<?php echo $_POST['fr_Ship_State'] ?>">
+								<input disabled＝"true" readonly＝"true" id="Ship_Zip" name="Ship_Zip" type="text" placehold="Zip" value="<?php echo $_POST['fr_Ship_Zip'] ?>">
 							</form>
 						</div>
 					</div>
@@ -125,17 +187,17 @@ $tempname=$_SESSION["username"];
 						<p>Bill Information</p>
 							<div class="form-one">
 								<form id="Bill_Info" disabled＝"true" readonly＝"true">
-									<input disabled＝"true" readonly＝"true" id="Bill_First_Name" name="Bill_First_Name" type="text" value="First Name: <?php echo $_POST['fr_Bill_First_Name'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_Last_Name" name="Bill_Last_Name" type="text" value="Last Name: <?php echo $_POST['fr_Bill_Last_Name'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_Address1" name="Bill_Address1" type="text" value="Address1: <?php echo $_POST['fr_Bill_Address1'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_Address2" name="Bill_Address2" type="text" value="Address2: <?php echo $_POST['fr_Bill_Address2'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_City" name="Bill_City" type="text" value="City: <?php echo $_POST['fr_Bill_City'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_State" name="Bill_State" type="text" value="State: <?php echo $_POST['fr_Bill_State'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_Zip" name="Bill_Zip" type="text" value="Zip: <?php echo $_POST['fr_Bill_Zip'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_Type" name="Bill_Type" type="text" value="Card Type: <?php echo $_POST['fr_Bill_Type'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_Number" name="Bill_Number" type="text" value="Card Number: <?php echo $_POST['fr_Bill_Number'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_Holder" name="Bill_Holder" type="text" value="Card Holder: <?php echo $_POST['fr_Bill_Holder'] ?>">
-									<input disabled＝"true" readonly＝"true" id="Bill_Expire" name="Bill_Expire" type="text" value="Expire Date: <?php echo $_POST['fr_Bill_Expire'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_First_Name" name="Bill_First_Name" type="text" placehold="First Name" value="<?php echo $_POST['fr_Bill_First_Name'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_Last_Name" name="Bill_Last_Name" type="text" placehold="Last Name" value="<?php echo $_POST['fr_Bill_Last_Name'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_Address1" name="Bill_Address1" type="text" placehold="Address1" value="<?php echo $_POST['fr_Bill_Address1'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_Address2" name="Bill_Address2" type="text" placehold="Address2" value="<?php echo $_POST['fr_Bill_Address2'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_City" name="Bill_City" type="text" placehold="City" value="<?php echo $_POST['fr_Bill_City'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_State" name="Bill_State" type="text" placehold="State" value="<?php echo $_POST['fr_Bill_State'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_Zip" name="Bill_Zip" type="text" placehold="Zip" value="<?php echo $_POST['fr_Bill_Zip'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_Type" name="Bill_Type" type="text" placehold="Card Type" value="<?php echo $_POST['fr_Bill_Type'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_Number" name="Bill_Number" type="text" placehold="Card Number" value="<?php echo $_POST['fr_Bill_Number'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_Holder" name="Bill_Holder" type="text" placehold="Card Holder" value="<?php echo $_POST['fr_Bill_Holder'] ?>">
+									<input disabled＝"true" readonly＝"true" id="Bill_Expire" name="Bill_Expire" type="text" placehold="Expire Date" value="<?php echo $_POST['fr_Bill_Expire'] ?>">
 								</form>
 							</div>
 						</div>
@@ -161,121 +223,34 @@ $tempname=$_SESSION["username"];
 					<tbody>
 					<?php
 							$username = $_SESSION['username'];
-							checkCart($username);
+							cartReview($username);
 					?>
-						<!-- <tr>
-							<td class="cart_product">
-								<a href=""><img src="images/cart/one.png" alt=""></a>
-							</td>
-							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
-							</td>
-							<td class="cart_price">
-								<p>$59</p>
-							</td>
-							<td class="cart_quantity">
-								<div class="cart_quantity_button">
-									<a class="cart_quantity_up" href=""> + </a>
-									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-									<a class="cart_quantity_down" href=""> - </a>
-								</div>
-							</td>
-							<td class="cart_total">
-								<p class="cart_total_price">$59</p>
-							</td>
-							<td class="cart_delete">
-								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-							</td>
-						</tr>
-
-						<tr>
-							<td class="cart_product">
-								<a href=""><img src="images/cart/two.png" alt=""></a>
-							</td>
-							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
-							</td>
-							<td class="cart_price">
-								<p>$59</p>
-							</td>
-							<td class="cart_quantity">
-								<div class="cart_quantity_button">
-									<a class="cart_quantity_up" href=""> + </a>
-									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-									<a class="cart_quantity_down" href=""> - </a>
-								</div>
-							</td>
-							<td class="cart_total">
-								<p class="cart_total_price">$59</p>
-							</td>
-							<td class="cart_delete">
-								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td class="cart_product">
-								<a href=""><img src="images/cart/three.png" alt=""></a>
-							</td>
-							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
-							</td>
-							<td class="cart_price">
-								<p>$59</p>
-							</td>
-							<td class="cart_quantity">
-								<div class="cart_quantity_button">
-									<a class="cart_quantity_up" href=""> + </a>
-									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-									<a class="cart_quantity_down" href=""> - </a>
-								</div>
-							</td>
-							<td class="cart_total">
-								<p class="cart_total_price">$59</p>
-							</td>
-							<td class="cart_delete">
-								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="4">&nbsp;</td>
-							<td colspan="2">
-								<table class="table table-condensed total-result">
-									<tr>
-										<td>Cart Sub Total</td>
-										<td>$59</td>
-									</tr>
-									<tr>
-										<td>Exo Tax</td>
-										<td>$2</td>
-									</tr>
-									<tr class="shipping-cost">
-										<td>Shipping Cost</td>
-										<td>Free</td>
-									</tr>
-									<tr>
-										<td>Total</td>
-										<td><span>$61</span></td>
-									</tr>
-								</table>
-							</td>
-						</tr> -->
 					</tbody>
 				</table>
 			</div>
 			<div class="payment-options">
-				<button id="btn_pay" name="btn_pay" onclick="pay()" >PAY</button>
-					<!-- <span>
-						<label><input type="checkbox"> Direct Bank Transfer</label>
-					</span>
-					<span>
-						<label><input type="checkbox"> Check Payment</label>
-					</span>
-					<span>
-						<label><input type="checkbox"> Paypal</label>
-					</span> -->
+				<form id="op_Info" action="orderConfirm.php" method="POST">
+					<button id="btn_pay" name="btn_pay" onclick="pay()" >PAY</button>
+					<input id="op_Ship_First_Name" name="op_Ship_First_Name" type="hidden">
+					<input id="op_Ship_Last_Name" name="op_Ship_Last_Name" type="hidden">
+					<input id="op_Ship_Address1" name="op_Ship_Address1" type="hidden">
+					<input id="op_Ship_Address2" name="op_Ship_Address2" type="hidden">
+					<input id="op_Ship_City" name="op_Ship_City" type="hidden">
+					<input id="op_Ship_State" name="op_Ship_State" type="hidden">
+					<input id="op_Ship_Zip" name="op_Ship_Zip" type="hidden">
+					<input id="op_Bill_First_Name" name="op_Bill_First_Name" type="hidden">
+					<input id="op_Bill_Last_Name" name="op_Bill_Last_Name" type="hidden">
+					<input id="op_Bill_Address1" name="op_Bill_Address1" type="hidden">
+					<input id="op_Bill_Address2" name="op_Bill_Address2" type="hidden">
+					<input id="op_Bill_City" name="op_Bill_City" type="hidden">
+					<input id="op_Bill_State" name="op_Bill_State" type="hidden">
+					<input id="op_Bill_Zip" name="op_Bill_Zip" type="hidden">
+					<input id="op_Bill_Type" name="op_Bill_Type" type="hidden">
+					<input id="op_Bill_Number" name="op_Bill_Number" type="hidden">
+					<input id="op_Bill_Holder" name="op_Bill_Holder" type="hidden">
+					<input id="op_Bill_Expire" name="op_Bill_Expire" type="hidden">
+				</form>
+					<button id="btn_back" name="btn_back" onclick="history.go(-1);" >Back</button>
 			</div>
 		</div>
 		</div>
